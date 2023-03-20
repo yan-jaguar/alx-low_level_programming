3357fae88454d5b18e6d485508e7f1109fc76a6e
yan-jaguar <yanchilenje@gmail.com>
2023-03-20 14:09:52 -0700

    Hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include <stdio.h> 
 /**
  * main - print line of code using puts
- *
+ * 
  * Return: 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingulaul puzzle"):
+	puts("\"Programming is like building a multilingual puzzle"):
 	return (0):
 }
