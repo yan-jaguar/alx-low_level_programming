78e95046995a32aa69cdd6ccb8c9b6cf17607614
yan-jaguar <yanchilenje@gmail.com>
2023-03-31 05:04:37 -0700

    update

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,7 +8,7 @@
 void print_number(int n)
 {
 	unsigned int m, d, count;
-	
+
 	if (n < 0)
 	{
 		_putchar(45);
@@ -20,13 +20,13 @@ void print_number(int n)
 	}
 	d = m;
 	count = 1;
-	
+
 	while (d > 9)
 	{
 		d /= 10;
 		count *= 10;
 	}
-	
+
 	for (; count >= 1; count /= 10)
 	{
 		_putchar(((m / count) % 10) + 48);
