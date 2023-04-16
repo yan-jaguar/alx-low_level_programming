9f2f9ddf7aeda2b4127d4d65e9e6b50600506700
yan-jaguar <yanchilenje@gmail.com>
2023-04-15 18:43:40 -0700

    update

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -2,10 +2,13 @@
 
 /**
  * _isdigit - checks for a digit (0 through 9)
- * @c: int to be checked
+ * @a: int to be checked
  * Return: 1 if c is a digit, 0 otherwise
  */
-int _isdigit(int c)
+int _isdigit(int a)
 {
-	return (c >= '0' && c <= '9');
+	if (a >= '0' && a <= '9')
+		return (1);
+	else
+		return (0);
 }
