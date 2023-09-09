c80187b2bb804beac33ea5ee71ec2621bd90478f
yan-jaguar <yankhochilenje@gmail.com>
2023-09-09 14:46:56 -0700

    task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	puts(\"Programming is like building a multilingual puzzle");
+		Return(0);
+}
