814f0d5adf4f1538c322cc128d3fc8084c0a2bd3
yan-jaguar <yankhochilenje@gmail.com>
2023-09-13 12:24:15 -0700

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alpha 10 time
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	char num;
+	char a;
+
+	for (num = '0' ; num <= '9' num++)
+	{
+		for (a = 'a' ; a <= 'z' a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n')
+	}
+}
