ba077194201e6b55665e8ea137485b55091563e7
yan-jaguar <yankhochilenje@gmail.com>
2023-09-21 03:11:19 -0700

    task0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - updates value of the variable
+ *The pointer points to 98
+ *@n: pointer to the value to be changed
+ *Return: 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+Pointers arrays strings
+Task0. takes a pointer to an int as parameter and updates the value it points to to 98.
