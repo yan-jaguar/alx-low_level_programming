9e41343ee7025cd39227592e339c1ce96342e3f9
yan-jaguar <yankhochilenje@gmail.com>
2023-09-21 11:27:39 -0700

    task9 correction

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -9,6 +9,7 @@ void _puts(char *str);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
 void rev_string(char *s);
 int _putchar(char c);
 
